095408126ddd05cb3d42756abc8e033ba0d315df
AsimaDzh <animasoon@gmail.com>
2026-01-06 19:25:53 +0600

    added teleportation to battle scene

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BattleSystem : MonoBehaviour
 {
@@ -39,5 +40,6 @@ public class BattleSystem : MonoBehaviour
     {
         Debug.Log("Battle Started!");
         _state = BattleState.InBattle;
+        SceneManager.LoadScene("BattleScene");
     }
 }
