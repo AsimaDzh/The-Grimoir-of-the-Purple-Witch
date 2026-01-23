2f72a45e41e986667d7c2178a097937fb29a34e6
AsimaDzh <animasoon@gmail.com>
2026-01-24 01:11:49 +0600

    Added player stats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static float playerHealth;
+    public static float playerMaxHealth;
+    public static float playerMana;
+    public static string[] spellCards;
+    public static string[] inventory;
+}
