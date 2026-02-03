1cd5652bc32249cf54bdfd01e7f941876b97afbb
AsimaDzh <animasoon@gmail.com>
2026-02-03 23:31:56 +0600

    Edited Input Manager. Added input actions

--- Assets/_Scripts/Core/InputManager.cs
+++ Assets/_Scripts/Core/InputManager.cs
@@ -1,14 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
 public class InputManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public static InputManager Instance { get; private set; }
 
-    void Update()
+    public InputActionAsset inputActions;
+
+    // Action Maps
+    private InputActionMap playerActionMap;
+    private InputActionMap uiActionMap;
+
+    // Player Actions
+    private InputAction moveAction;
+    private InputAction attackAction;
+    private InputAction interactAction;
+    private InputAction pauseAction;
+    private InputAction cancelAction;
+
+    public Vector2 MoveInput { get; private set; }
+    public bool AttackPressed { get; private set; }
+    public bool InteractPressed { get; private set; }
+
+
+    public Action OnAttackPressed;
+    public Action OnInteractPressed;
+    public Action OnPausePressed;
+    public Action OnCancelPressed;
+
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
