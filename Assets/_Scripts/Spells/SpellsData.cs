383b9d6afe1603cc9ebfac07c1a9d165336ff999
AsimaDzh <animasoon@gmail.com>
2026-02-23 12:38:58 +0600

    Edit: Add spell types to SpellsData script

--- Assets/_Scripts/Spells/SpellsData.cs
+++ Assets/_Scripts/Spells/SpellsData.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
-public class SpellsData : MonoBehaviour
-{
 
+[CreateAssetMenu(
+    fileName = "SpellsData",
+    menuName = "GameData/SpellsData",
+    order = 1)]
+
+public class SpellsData : ScriptableObject
+{
+    public enum SpellType
+    {
+        Single = 0,
+        Multiple = 1,
+        Buff = 2,
+        Heal = 3,
+        Defensive = 4,
+        Poisonous = 5
+    }
 }
