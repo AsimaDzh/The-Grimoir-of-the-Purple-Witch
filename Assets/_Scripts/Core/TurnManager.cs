774e3f706699702299b6b7add7cf81921390fe94
Lemonka <animasoon@gmail.com>
2026-03-14 17:34:35 +0500

    Edit: Add TurnState and methods to TurnManager.cs

--- Assets/_Scripts/Core/TurnManager.cs
+++ Assets/_Scripts/Core/TurnManager.cs
@@ -1,6 +1,42 @@
 using UnityEngine;
 
+public enum TurnState
+{
+    PlayerTurn = 0,
+    EnemyTurn = 1,
+    Busy = 2
+}
+
 public class TurnManager : MonoBehaviour
 {
+    public static TurnManager Instance { get; private set; }
+    public TurnState CurrentTurn;
 
+    public bool isPlayerTurn => CurrentTurn == TurnState.PlayerTurn;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public void StartPlayerTurn()
+    {
+        CurrentTurn = TurnState.PlayerTurn;
+        Debug.Log("Player's Turn Started");
+    }
+
+
+    public void StartEnemyTurn()
+    {
+        CurrentTurn = TurnState.EnemyTurn;
+        Debug.Log("Enemy's Turn Started");
+    }
 }
